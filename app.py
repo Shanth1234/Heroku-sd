728f1fd9bf2de84aa8f699f351395f7c6ac8850c
Shanth1234 <96223229+Shanth1234@users.noreply.github.com>
2022-02-19 17:18:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ import pandas as pd
 app = dash.Dash(__name__)
 server = app.server
 
-df = pd.read_csv('https://raw.githubusercontent.com/nethajinirmal13/Training-datasets/main/matches.csv')
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
 
-fig = px.bar(df, x="winner", y="win_by_runs",
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
                  size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60,)
+                 log_x=True, size_max=60)
 
 app.layout = html.Div([
     dcc.Graph(
-        id='Best winning teams based on the win by runs',
+        id='life-exp-vs-gdp',
         figure=fig
     )
 ])
@@ -25,4 +25,3 @@ app.layout = html.Div([
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
