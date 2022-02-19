b0ea23ab3184687ffdf00e3334c97dad770c69cd
Shanth1234 <96223229+Shanth1234@users.noreply.github.com>
2022-02-19 16:25:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import dash
+""""import dash
 import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
@@ -29,5 +29,33 @@ app.layout = html.Div([
 def update_value(value_list):
     return value_list[0]*value_list[1]
  
+if __name__ == '__main__':
+    app.run_server(debug=True)""""
+
+import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+
+app = dash.Dash(__name__)
+server = app.server
+
+df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
+
+fig = px.scatter(df, x="gdp per capita", y="life expectancy",
+                 size="population", color="continent", hover_name="country",
+                 log_x=True, size_max=60)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='life-exp-vs-gdp',
+        figure=fig
+    )
+])
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
